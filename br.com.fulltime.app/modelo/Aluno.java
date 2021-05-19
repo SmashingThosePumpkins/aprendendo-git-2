1340a9b25ae0625776c8be66ceb47a49f07f034e
Pablo A <pablo.silva@ftdata.com.br>
2021-05-19 10:50:21 -0300

    Adicionada a classe "Aluno", seu construtor e métodos getter

---
+++ br.com.fulltime.app/modelo/Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    public Aluno (String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
